f038ed97446fb92d5818ba3b70d5c935272f60af
Arpple <rbo_sqat@hotmail.com>
2017-03-07 11:53:44 +0700

    add map

---
+++ Assets/Scripts/Test/Editor/Map/Map.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace End.Test
+{
+	public class Map
+	{
+		private End.Map _map;
+
+		[SetUp]
+		public void Init()
+		{
+			_map = ScriptableObject.CreateInstance<End.Map>();
+		}
+
+		public void Create()
+		{
+			_map.SetMap(5, 5, Tile.Grass);
+
+			var pos = new End.MapPositionComponent();
+			pos.X = 5;
+			pos.Y = 5;
+
+			Assert.IsTrue(
+				_map.GetTile(5, 5) == Tile.Grass
+				&& _map.GetTile(pos) == Tile.Grass
+			);
+		}
+	}
+}
+
