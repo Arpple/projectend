de79728a9e26ebfec98ede9c527ee7b8e607acf2
Arpple <rbo_sqat@hotmail.com>
2017-03-07 18:50:50 +0700

    fix map test
    - add overload method SetTile

--- Assets/Scripts/Test/Editor/Map/Map.cs
+++ Assets/Scripts/Test/Editor/Map/Map.cs
@@ -13,18 +13,28 @@ namespace End.Test
 			_map = ScriptableObject.CreateInstance<End.Map>();
 		}
 
+		[Test]
 		public void Create()
 		{
 			_map.SetMap(5, 5, Tile.Grass);
 
 			var pos = new End.MapPositionComponent();
-			pos.X = 5;
-			pos.Y = 5;
+			pos.X = 4;
+			pos.Y = 4;
 
 			Assert.IsTrue(
-				_map.GetTile(5, 5) == Tile.Grass
+				_map.GetTile(4, 4) == Tile.Grass
 				&& _map.GetTile(pos) == Tile.Grass
 			);
+		}
+
+		[Test]
+		public void SetTile()
+		{
+			_map.SetMap(5, 5, Tile.Grass);
+			_map.SetTile(1, 1, Tile.None);
+
+			Assert.IsTrue(_map.GetTile(1, 1) == Tile.None);
 		}
 	}
 }
