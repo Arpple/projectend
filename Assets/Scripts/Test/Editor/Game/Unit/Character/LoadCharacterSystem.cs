7ffa01b1f20fd0ea3cec20518d6f9c841111ab2c
Arpple <rbo_sqat@hotmail.com>
2017-03-11 16:39:21 +0700

    add test loadcharacter system

---
+++ Assets/Scripts/Test/Editor/Game/Unit/Character/LoadCharacterSystem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace End.Test
+{
+	public class LoadCharacterSystem
+	{
+		private Contexts _contexts;
+		private End.CharacterSetting _setting;
+
+		[SetUp]
+		public void Init()
+		{
+			_contexts = TestHelper.CreateContexts();
+
+			GameSetting setting = Resources.Load<GameSetting>("Game/Core/Setting/GameSetting");
+			_setting = setting.UnitSetting.CharacterSetting;
+			Assert.IsNotNull(_setting);
+		}
+
+		[Test]
+		public void EditorTest()
+		{
+			var system = new End.LoadCharacterSystem(_contexts, _setting);
+
+			var entity = _contexts.game.CreateEntity();
+			entity.AddCharacter(Character.LastBoss);
+
+			system.Execute();
+
+			Assert.IsTrue(entity.hasResource);
+		}
+	}
+}
+
