e155828bdb505e579a08c10cd2d317f8a402bb36
Arpple <rbo_sqat@hotmail.com>
2017-03-11 23:15:43 +0700

    add lounge dummy function

---
+++ Assets/Scripts/Game/UI/Lounge/LoungeConnectionDialogue.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+namespace End
+{
+	public class LoungeConnectionDialogue : MonoBehaviour
+	{
+		public Text IpInputField;
+
+		public string GetIpAddress()
+		{
+			return IpInputField.text;
+		}
+
+		public void BackButtonClick()
+		{
+
+		}
+
+		public void JoinButtonClick()
+		{
+
+		}
+
+		/// <summary>
+		/// when this dialogue is show
+		/// </summary>
+		public void Show()
+		{
+			gameObject.SetActive(true);
+		}
+
+		/// <summary>
+		/// when this dialogue is close
+		/// </summary>
+		public void Close()
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/Game/UI/Lounge/LoungeNetworkManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace End
+{
+	public class LoungeNetworkManager : NetworkLobbyManager
+	{
+		public LoungeConnectionDialogue ConnectionDialouge;
+
+		public Button HostButton;
+		public Button JoinButton;
+
+		/// <summary>
+		/// click on Host
+		/// </summary>
+		public void HostButtonClick()
+		{
+			Debug.Log("Start Host");
+		}
+
+		/// <summary>
+		/// click on Join
+		/// </summary>
+		public void JoinButtonClick()
+		{
+			ShowConnectionDialogue();
+		}
+
+		/// <summary>
+		/// in dialogue click cancel
+		/// </summary>
+		public void ConnectionDialogueCancel()
+		{
+			CloseConnectionDialogue();
+		}
+
+		/// <summary>
+		/// in dialogue click ok
+		/// </summary>
+		public void ConnectionDialogueConfirm()
+		{
+			CloseConnectionDialogue();
+			Debug.Log("join server : " + ConnectionDialouge.GetIpAddress());
+		}
+
+		private void ShowConnectionDialogue()
+		{
+			ConnectionDialouge.Show();
+			HostButton.enabled = false;
+			JoinButton.enabled = false;
+		}
+
+		private void CloseConnectionDialogue()
+		{
+			ConnectionDialouge.Close();
+			HostButton.enabled = true;
+			JoinButton.enabled = true;
+		}
+
+
+	}
+
+}
