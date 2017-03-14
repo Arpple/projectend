f77674c622c217e317c9d3819e45600bb14ce3e4
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2017-03-15 01:14:44 +0700

    + Add RoleContent in CharactorScence. + Add dummy sprite icon of each role.

--- Assets/Scripts/Game/UI/CharacterSelection/CharacterSelectionController.cs
+++ Assets/Scripts/Game/UI/CharacterSelection/CharacterSelectionController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using End.Game.UI.CharacterSelection;
+using UnityEngine.UI;
+
 namespace End.UI.CharacterSelection {
     public class CharacterSelectionController : MonoBehaviour{
         public UnitStatus UnitStatus;
         public UnitSkill UnitSkill;
-        //public Text
-        //public List<>
+
+        public GameObject RoleContent,CharacterContent;
+
         void Start() {
             InitialPrefabs();
         }
@@ -32,5 +35,45 @@ namespace End.UI.CharacterSelection {
             //TODO : Set Character Ability
             //UnitSkill.SetAbility();
         }
+
+        #region UI RoleContent
+
+        public Text RoleTitle, RoleDesciption;
+        public Image RoleImage;
+
+        public void ToggleViewRole() {
+            bool isActive = this.RoleContent.activeInHierarchy;
+            this.RoleContent.SetActive(!isActive);
+            //this.CharacterContent.SetActive(isActive);
+        }
+
+        public void GodRole() {
+            RoleTitle.text = "- "+RoleAndDescription.GOD_NAME+" -";
+            RoleDesciption.text = "Win Condition"+ Environment.NewLine
+                + RoleAndDescription.GOD_WIN_CONDITION;
+            RoleImage.sprite = Resources.Load<Sprite>(RoleAndDescription.ICON_PATH_GOD);
+        }
+
+        public void ApostleRole() {
+            RoleTitle.text = "- " + RoleAndDescription.APOSTLE_NAME + " -";
+            RoleDesciption.text = "Win Condition" + Environment.NewLine
+                + RoleAndDescription.APOSTLE_WIN_CONDITION;
+            RoleImage.sprite = Resources.Load<Sprite>(RoleAndDescription.ICON_PATH_APOSTLE);
+        }
+
+        public void PriestRole() {
+            RoleTitle.text = "- " + RoleAndDescription.PRIEST_NAME + " -";
+            RoleDesciption.text = "Win Condition" + Environment.NewLine
+                + RoleAndDescription.PRIEST_WIN_CONDITION;
+            RoleImage.sprite = Resources.Load<Sprite>(RoleAndDescription.ICON_PATH_PRIEST);
+        }
+
+        public void AtheistRole() {
+            RoleTitle.text = "- " + RoleAndDescription.ATHEIST_NAME + " -";
+            RoleDesciption.text = "Win Condition" + Environment.NewLine
+                + RoleAndDescription.ATHEIST_WIN_CONDITION;
+            RoleImage.sprite = Resources.Load<Sprite>(RoleAndDescription.ICON_PATH_ATHEIST);
+        }
+        #endregion
     }
 }
