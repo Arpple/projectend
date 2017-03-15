fc862889a8a41518728da1243358bca6ae2b1449
Arpple <rbo_sqat@hotmail.com>
2017-03-15 21:15:47 +0700

    lobby start network from lounge data

---
+++ Assets/Scripts/Lobby/LobbyController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.Assertions;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace End.Network
+{
+	public class LobbyController : NetworkLobbyManager
+	{
+		public static LobbyController Instance;
+
+		public Lounge.LoungeToLobby LoungeData
+		{
+			get { return Lounge.LoungeToLobby.Instance; }
+		}
+
+		private void Awake()
+		{
+			Instance = this;
+
+			Assert.IsNotNull(LoungeData);
+		}
+
+		private void Start()
+		{
+			if(LoungeData.IsHost)
+			{
+				StartHost();
+			}
+			else
+			{
+				networkAddress = LoungeData.ConnectingIpAddress;
+				StartClient();
+			}
+
+			Destroy(LoungeData.gameObject);
+		}
+
+		#region Client
+		#endregion
+
+		#region Server
+		public override void OnLobbyStartHost()
+		{
+			base.OnLobbyStartHost();
+			maxConnections = maxPlayers;
+		}
+		#endregion
+	}
+}
