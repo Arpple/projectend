70424bfc09e9807a56f711e6658f9b274a8f6fc5
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2017-03-17 00:20:39 +0700

    Add new Tile. Space and Snow

---
+++ Assets/Scripts/Tool/MapEditor/MapEditorFillTile.cs
@@ -0,0 +1,36 @@
+﻿using End.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace End.MapEditor {
+    public class MapEditorFillTile : MonoBehaviour{
+        public MapEditorToolkits toolKits;
+        public MapEditorController mapEditor;
+
+        public GameObject contentView;
+
+        public InputField from_x, from_y, to_x, to_y;
+        
+        public void FillTile() {
+            int from_x=Int32.Parse(this.from_x.text), from_y=Int32.Parse(this.from_y.text);
+            int to_x=Int32.Parse(this.to_x.text),to_y=Int32.Parse(this.to_y.text);
+
+            Tile tile = TileBrushSystem.TileBrush.TileType;
+
+            for(int y = from_y; y <= to_y; y++) {
+                for(int x = from_x; x <= to_x; x++) {
+                    Debug.Log("Fill Tile ["+x+","+y+"] with ["+tile.ToString()+"]" );
+                    mapEditor.LoadingMap.SetTile(x, y, tile);
+                }
+            }
+        }
+
+        public void Toogle() {
+            this.contentView.SetActive(!this.contentView.gameObject.activeInHierarchy);
+        }
+    }
+}
