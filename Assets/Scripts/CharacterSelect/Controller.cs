3d64efb6450a8e5bbf8c485576ffa470337bab55
Arpple <rbo_sqat@hotmail.com>
2017-03-18 01:03:25 +0700

    charselect controller template

---
+++ Assets/Scripts/CharacterSelect/Controller.cs
@@ -0,0 +1,57 @@
+﻿using Entitas;
+using UnityEngine;
+
+namespace End.CharacterSelect
+{
+	public class Controller : MonoBehaviour
+	{
+		public Controller Instance;
+
+		private Systems _systems;
+		private Contexts _contexts;
+		private bool _isInit;
+
+		private void Awake()
+		{
+			Instance = this;
+			_isInit = false;
+		}
+
+		public void Start()
+		{
+			var netCon = NetworkController.Instance;
+			netCon.OnAllPlayerReadyCallback += Initialize;
+			_contexts = Contexts.sharedInstance;
+			
+		}
+
+		public void Initialize()
+		{
+			_contexts.SetAllContexts();
+			_systems = CreateSystems(_contexts);
+
+			_systems.Initialize();
+			_isInit = true;
+		}
+
+		private void Update()
+		{
+			if (!_isInit) return;
+
+			_systems.Execute();
+			_systems.Cleanup();
+		}
+
+		private void OnDestroy()
+		{
+			_systems.TearDown();
+		}
+
+		Systems CreateSystems(Contexts contexts)
+		{
+			return new Feature("Systems")
+				.Add(new Game.ClearContextsSystem(contexts));
+		}
+	}
+
+}
