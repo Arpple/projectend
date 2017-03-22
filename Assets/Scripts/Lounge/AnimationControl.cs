8c312dcb9eee79d0c84b7af1e9dc2cda8506e8c2
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2017-03-22 21:17:21 +0700

    Change Lounge UI

---
+++ Assets/Scripts/Lounge/AnimationControl.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+namespace End.Lounge {
+    public class AnimationControl : MonoBehaviour{
+        public Animator Animator;
+
+        public void Play() {
+            this.Animator.Play("MoveUp",0);
+        }
+    }
+}
