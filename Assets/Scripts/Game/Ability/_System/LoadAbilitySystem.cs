f7894a1d8dffe7224263f1c47248be9ae7f67c6c
Arpple <rbo_sqat@hotmail.com>
2017-03-22 18:40:03 +0700

    seperate ability creating to LoadAbilitySystem

---
+++ Assets/Scripts/Game/Ability/_System/LoadAbilitySystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Entitas;
+
+namespace End.Game
+{
+	public class LoadAbilitySystem : ReactiveSystem<GameEntity>
+	{
+		readonly CacheList<string, Ability> _ability;
+
+		public LoadAbilitySystem(Contexts contexts)
+			: base(contexts.game)
+		{
+			_ability = new CacheList<string, Ability>();
+		}
+
+		protected override Collector<GameEntity> GetTrigger(IContext<GameEntity> context)
+		{
+			return context.CreateCollector(GameMatcher.Ability, GroupEvent.Added);
+		}
+
+		protected override bool Filter(GameEntity entity)
+		{
+			return entity.hasAbility
+				&& entity.ability.AbilityClassName != ""
+				&& entity.ability.Ability == null;
+		}
+
+		protected override void Execute(List<GameEntity> entities)
+		{
+			foreach(var e in entities)
+			{
+				e.ability.Ability = _ability.Get(e.ability.AbilityClassName, (name)
+					=> (Ability)System.Reflection.Assembly.GetExecutingAssembly().CreateInstance(name));
+			}
+		}
+	}
+
+}
--- Assets/Scripts/Game/Deck/Card/_System/LoadCardSystem.cs
+++ Assets/Scripts/Game/Deck/Card/_System/LoadCardSystem.cs
@@ -10,13 +10,10 @@ namespace End.Game
 	{
 		readonly CardSetting _setting;
 
-		private CacheList<string, Ability> _ability;
-
 		public LoadCardSystem(Contexts contexts, CardSetting setting)
 			: base(contexts)
 		{
 			_setting = setting;
-			_ability = new CacheList<string, Ability>();
 		}
 
 		protected override Collector<GameEntity> GetTrigger(IContext<GameEntity> context)
@@ -32,20 +29,6 @@ namespace End.Game
 		protected override Blueprint GetBlueprint(GameEntity entity)
 		{
 			return _setting.GetCardBlueprint(entity.card.Type);
-		}
-
-		protected override void Execute(List<GameEntity> entities)
-		{
-			base.Execute(entities);
-			foreach (var e in entities)
-			{
-				if(e.hasAbility)
-				{
-					var ability = _ability.Get(e.ability.AbilityClassName, (name)
-						=> (Ability)System.Reflection.Assembly.GetExecutingAssembly().CreateInstance(name));
-					e.ability.Ability = ability;
-				}
-			}
 		}
 	}
 }
