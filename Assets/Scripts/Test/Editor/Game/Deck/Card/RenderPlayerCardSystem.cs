3fc306e9e3a409ea55d67c1d9665d2c39b9da7c4
Arpple <rbo_sqat@hotmail.com>
2017-03-23 22:36:14 +0700

    add test for RenderPlayerCardSystem

---
+++ Assets/Scripts/Test/Editor/Game/Deck/Card/RenderPlayerCardSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using End.Game;
+using End.Game.UI;
+
+namespace End.Test
+{
+	public class TestRenderPlayerCardSystem
+	{
+		private Contexts _contexts;
+
+		[SetUp]
+		public void Init()
+		{
+			_contexts = TestHelper.CreateContexts();
+		}
+
+		[Test]
+		public void CardViewObjectMovedToDeckObject()
+		{
+			var system = new RenderPlayerCardSystem(_contexts);
+
+			var player = new GameObject().AddComponent<Player>();
+			player.PlayerId = 1;
+
+			var container = _contexts.game.CreateEntity();
+			container.AddPlayer(player);
+			container.AddPlayerDeck(new GameObject());
+			
+			var card = _contexts.game.CreateEntity();
+			card.AddPlayerCard(1);
+			card.AddView(new GameObject());
+
+			system.Execute();
+
+			Assert.AreEqual(container.playerDeck.PlayerDeckObject, card.view.GameObject.transform.parent.gameObject);
+		}
+	}
+
+}
+
