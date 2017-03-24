32ac9dd38ae66711b8938282711e0fd9bcb4c31e
Arpple <rbo_sqat@hotmail.com>
2017-03-24 20:46:50 +0700

    card base loading

---
+++ Assets/Scripts/Game/Deck/CardUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace End.Game
+{
+	public class CardUI : MonoBehaviour, ICustomView
+	{
+		public Image MainImage;
+
+		public GameObject CreateView(GameEntity entity, Sprite sprite)
+		{
+			SetImage(sprite);
+
+			return gameObject;
+		}
+
+		public void SetImage(Sprite image)
+		{
+			MainImage.sprite = image;
+		}
+	}
+
+}
