f4a3291d619c29b5665152b199198589ee599b97
Arpple <rbo_sqat@hotmail.com>
2017-03-25 15:37:44 +0700

    add EntityGroup shortcut

---
+++ Assets/Scripts/Game/Core/EntityGroup.cs
@@ -0,0 +1,38 @@
+﻿using Entitas;
+
+namespace End.Game
+{
+	public static class EntityGroup
+	{
+		private static GameContext _game
+		{
+			get { return Contexts.sharedInstance.game; }
+		}
+
+		public static GameEntity[] Player
+		{
+			get { return _game.GetEntities(GameMatcher.Player); }
+		}
+
+		public static GameEntity LocalPlayer
+		{
+			get { return _game.GetGroup(GameMatcher.LocalPlayer).GetSingleEntity(); }
+		}
+
+		public static GameEntity[] Character
+		{
+			get { return _game.GetEntities(GameMatcher.Character); }
+		}
+
+		public static GameEntity[] Card
+		{
+			get { return _game.GetEntities(GameMatcher.Card); }
+		}
+
+		public static GameEntity[] Tile
+		{
+			get { return _game.GetEntities(GameMatcher.Tile); }
+		}
+	}
+
+}
