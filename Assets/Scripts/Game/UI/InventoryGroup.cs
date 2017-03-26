924c6266e98f6609aa36cab5a86c472b4727de86
Arpple <rbo_sqat@hotmail.com>
2017-03-26 23:26:13 +0700

    fix scene error

--- Assets/Scripts/Game/UI/InventoryGroup.cs
+++ Assets/Scripts/Game/UI/InventoryGroup.cs
@@ -8,7 +8,6 @@ namespace End.Game.UI
 	[Serializable]
 	public class InventoryGroup
 	{
-		public Text Text; //? change name?
 		public CardContainer CardContainer;
 		public GameObject BoxContainer;
 		public GameObject SkillContainer;
@@ -18,14 +17,8 @@ namespace End.Game.UI
 			Assert.IsNotNull(CardContainer);
 			Assert.IsNotNull(BoxContainer);
 			Assert.IsNotNull(SkillContainer);
-			Assert.IsNotNull(Text);
 
 			CardContainer.Awake();
-		}
-
-		public void SetText(string text)
-		{
-			Text.text = text;
 		}
 	}
 
