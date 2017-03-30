8ac0bc8ee3d1dc5bdce173658f401d9560e4fcfe
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2017-03-30 16:40:51 +0700

    Add Unit Animation. Hurt & Attack(Beta)

---
+++ Assets/Scripts/_Sources/Game/Unit/UnitAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class UnitAnimation : MonoBehaviour {
+    public Animator Animator;
+
+    public enum Direction{ Top,Down,Left,Right};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
