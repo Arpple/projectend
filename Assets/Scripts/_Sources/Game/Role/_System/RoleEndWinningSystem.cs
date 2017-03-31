1bc8bd9bc27159c732ad1b948890befa47db8643
Arpple <rbo_sqat@hotmail.com>
2017-03-31 22:08:19 +0700

    role end winning

---
+++ Assets/Scripts/_Sources/Game/Role/_System/RoleEndWinningSystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using Entitas;
+using System;
+using System.Collections.Generic;
+
+namespace End.Game
+{
+	public class RoleEndWinningSystem : ReactiveSystem<GameEntity>
+	{
+		public RoleEndWinningSystem(Contexts contexts) : base(contexts.game)
+		{
+
+		}
+
+		protected override Collector<GameEntity> GetTrigger(IContext<GameEntity> context)
+		{
+			return context.CreateCollector(GameMatcher.Dead, GroupEvent.Added);
+		}
+
+		protected override bool Filter(GameEntity entity)
+		{
+			return entity.isDead && entity.unit.OwnerEntity.role.RoleObject is RoleEnd;
+		}
+
+		protected override void Execute(List<GameEntity> entities)
+		{
+			foreach(var e in entities)
+			{
+				e.unit.OwnerEntity.isWin = true;
+			}
+		}
+	}
+}
+
