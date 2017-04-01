6fd68a1af6e6f79b9595776aa1b4f94685004265
Arpple <rbo_sqat@hotmail.com>
2017-04-02 00:37:56 +0700

    status ui object base

---
+++ Assets/Scripts/_Sources/Game/UI/UnitStatusPanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using End.UI;
+using UnityEngine.Assertions;
+
+namespace End.Game.UI
+{
+	public class UnitStatusPanel : MonoBehaviour
+	{
+		[Header("Player and Character")]
+		public Text PlayerNameText;
+		public Text CharacterNameText;
+		public Icon CharacterIcon;
+
+		[Header("Character Status")]
+		public Text DeckCardCountText;
+		public Text BoxCardCountText;
+		public Text AttackPowerText;
+		public Text AttackRangeText;
+		public Text MoveSpeedText;
+		public Text VisionRangeText;
+
+		private void Awake()
+		{
+			Assert.IsNotNull(PlayerNameText);
+			Assert.IsNotNull(CharacterNameText);
+			Assert.IsNotNull(CharacterIcon);
+			Assert.IsNotNull(DeckCardCountText);
+			Assert.IsNotNull(BoxCardCountText);
+			Assert.IsNotNull(AttackPowerText);
+			Assert.IsNotNull(AttackRangeText);
+			Assert.IsNotNull(MoveSpeedText);
+			Assert.IsNotNull(VisionRangeText);
+		}
+
+
+
+	}
+}
