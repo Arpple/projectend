0124f4d2472c26ef0da7c88aa5f4a601d600342e
Arpple <rbo_sqat@hotmail.com>
2017-04-03 00:57:05 +0700

    add hp bar

---
+++ Assets/Scripts/_Sources/Game/UI/HpBar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace End.Game.UI
+{
+	public class HpBar : MonoBehaviour
+	{
+		public Image CurrentValueBarImage;
+		public Text ValueText;
+
+		private int _maxValue;
+
+		public void SetMaxValue(int x)
+		{
+			_maxValue = x;
+		}
+
+		public void SetCurrentValue(int x)
+		{
+			ValueText.text = x + "/" + _maxValue;
+			UpdateValueBar(x);
+		}
+
+		public void UpdateValueBar(int current)
+		{
+			var scale = _maxValue > 0
+				? ((float)current) / _maxValue
+				: 0;
+
+			CurrentValueBarImage.transform.localScale = new Vector3(scale, 1, 0);
+		}
+	}
+
+}
