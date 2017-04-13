566470ec9b02bc29424f54ec4a9e5b96813cee8a
Arpple <rbo_sqat@hotmail.com>
2017-04-13 21:23:36 +0700

    resource collect ability

---
+++ Assets/Scripts/Test/Editor/Card/Ability/AbilityCollectTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Test.CardTest.AbilityTest
+{
+	public class AbilityCollectTest : ActiveAbilityTest<TileEntity>
+	{
+		protected override Ability CreateAbility()
+		{
+			return new AbilityCollect();
+		}
+
+		protected override TileEntity SetupTarget()
+		{
+			var entity = _contexts.tile.CreateEntity();
+			entity.AddCharge(1);
+			return entity;
+		}
+
+		public void OnTargetSelected_TargetHaveCharge_TargetChargeMinusByOne()
+		{
+			_activeAbility.OnTargetSelected(_caster, _target);
+			Assert.AreEqual(0, _target.charge.Count);
+		}
+	}
+}
---
+++ Assets/Scripts/_Sources/Card/Ability/Deck/AbilityCollect.cs
@@ -0,0 +1,17 @@
+﻿public class AbilityCollect : ActiveAbility<TileEntity>
+{
+	public override TileEntity[] GetTilesArea(UnitEntity caster)
+	{
+		return TileAreaSelector.GetAllInRange(caster.GetTileOfUnit(), 1);
+	}
+
+	public override TileEntity GetTargetFromSelectedTile(UnitEntity caster, TileEntity tile)
+	{
+		return tile.hasCharge && tile.charge.Count > 0 ? tile : null;
+	}
+
+	public override void OnTargetSelected(UnitEntity caster, TileEntity target)
+	{
+		target.ReplaceCharge(target.charge.Count - 1);
+	}
+}
