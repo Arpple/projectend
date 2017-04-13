7ee1b8c1f8debf3b4be1fe7a9d59d954dae449e6
Arpple <rbo_sqat@hotmail.com>
2017-04-13 20:13:23 +0700

    resource create system

---
+++ Assets/Scripts/Test/Editor/Tile/TileResourceCreatingSystemTest.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+
+namespace Test.TileTest
+{
+	public class TileResourceCreatingSystemTest : ContextsTest
+	{
+		[SetUp]
+		public void Init()
+		{
+			_systems.Add(new TileResourceCreatingSystem(_contexts));
+		}
+
+		private TileEntity CreateTileEntity(Tile type)
+		{
+			var entity = _contexts.tile.CreateEntity();
+			entity.AddTile(type);
+			return entity;
+		}
+
+		private void AssertTileHasResouces(TileEntity tile, Resource type)
+		{
+			Assert.IsTrue(tile.hasResource);
+			Assert.AreEqual(type, tile.resource.Type);
+		}
+
+		[Test]
+		public void Initialize_DeepForestTile_WoodResourceAdded()
+		{
+			var tile = CreateTileEntity(Tile.DeepForest);
+			_systems.Initialize();
+			AssertTileHasResouces(tile, Resource.Wood);
+		}
+
+		[Test]
+		public void Initialize_WaterTile_WaterResourceAdded()
+		{
+			var tile = CreateTileEntity(Tile.Water);
+			_systems.Initialize();
+			AssertTileHasResouces(tile, Resource.Water);
+		}
+
+		[Test]
+		public void Initialize_MountainTile_CoalResourceAdded()
+		{
+			var tile = CreateTileEntity(Tile.Mountain);
+			_systems.Initialize();
+			AssertTileHasResouces(tile, Resource.Coal);
+		}
+
+		private void NoResourceTileTest(Tile tile)
+		{
+			var entity = CreateTileEntity(tile);
+			_systems.Initialize();
+			Assert.IsFalse(entity.hasResource);
+		}
+
+		[Test]
+		public void Initialize_OtherTile_NoResource()
+		{
+			NoResourceTileTest(Tile.Desert);
+			NoResourceTileTest(Tile.Grass);
+			NoResourceTileTest(Tile.SnowMountain);
+			NoResourceTileTest(Tile.None);
+			NoResourceTileTest(Tile.Snow);
+			NoResourceTileTest(Tile.Space);
+		}
+	}
+}
---
+++ Assets/Scripts/_Sources/Tile/_System/TileResourceCreatingSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entitas;
+
+public class TileResourceCreatingSystem : IInitializeSystem
+{
+	private TileContext _context;
+
+	public TileResourceCreatingSystem(Contexts contexts)
+	{
+		_context = contexts.tile;
+	}
+
+	public void Initialize()
+	{
+		foreach (var tile in _context.GetEntities(TileMatcher.Tile))
+		{
+			AddTileResource(tile);
+		}
+	}
+
+	private void AddTileResource(TileEntity entity)
+	{
+		switch(entity.tile.Type)
+		{
+			case Tile.DeepForest:
+				entity.AddResource(Resource.Wood);
+				break;
+
+			case Tile.Mountain:
+				entity.AddResource(Resource.Coal);
+				break;
+
+			case Tile.Water:
+				entity.AddResource(Resource.Water);
+				break;
+		}
+	}
+}
