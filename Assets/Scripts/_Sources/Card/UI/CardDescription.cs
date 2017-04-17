f105ab16c965bc1934192cd61fba428c9981d028
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2017-04-18 00:06:49 +0700

    Add Animation on Active/Passive Button in Card Description

--- Assets/Scripts/_Sources/Card/UI/CardDescription.cs
+++ Assets/Scripts/_Sources/Card/UI/CardDescription.cs
@@ -7,10 +7,22 @@ public class CardDescription : MonoBehaviour
 	public Text CardName;
 	public Text CardDetail;
 
-	public void Init()
+    [Header("Animator")]
+    public Animator Animator;
+
+    [Header("Button Panel")]
+    public Animator ButtonGroup_Animator;
+    private bool onShowPassive;
+    public GameObject ButtonGroup;
+    public Button ActiveButton;
+    public Button PassiveButton;
+
+
+    public void Init()
 	{
 		Assert.IsNotNull(CardName);
 		Assert.IsNotNull(CardDetail);
+        ShowActiveDescription();
 	}
 
 	public void SetDescription(CardObject card)
@@ -18,10 +30,39 @@ public class CardDescription : MonoBehaviour
 		//TODO: get description string
 		CardName.text = card.name;
 		CardDetail.text = "Desc " + card.name;
-	}
+        Animator.Play("SwapDescription");
+    }
 
 	public void ToggleVisibility()
 	{
 		gameObject.SetActive(!gameObject.activeSelf);
 	}
+    public void ReButtonOrder(string buttonName) {
+        Transform b = ButtonGroup.transform.FindChild(buttonName);
+        if(b != null) {
+            b.transform.SetAsLastSibling();
+        }
+    }
+
+    public void ShowActiveDescription() {
+        if(onShowPassive) {
+            ReButtonOrder(ActiveButton.name);
+            ButtonGroup_Animator.Play("FocusActive");
+            Animator.Play("SwapDescription");
+        }
+        onShowPassive = false;
+    }
+
+    public void ShowPassiveDescription() {
+        if(!onShowPassive) {
+            ReButtonOrder(PassiveButton.name);
+            ButtonGroup_Animator.Play("FocusPassive");
+            Animator.Play("SwapDescription");
+        }
+        onShowPassive = true;
+    }
+
+    public void ReorderButtonToTop(Button button) {
+        button.transform.SetAsLastSibling();
+    }
 }
