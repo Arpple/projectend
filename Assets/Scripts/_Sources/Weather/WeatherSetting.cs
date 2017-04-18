fd24e81300fbec13f1514a55d9401cad52f9d54d
Arpple <rbo_sqat@hotmail.com>
2017-04-18 14:26:08 +0700

    add weather setting

---
+++ Assets/Scripts/_Sources/Weather/WeatherSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class WeatherSetting
+{
+	/// <summary>
+	/// random weigth for resource count to cost for resolving weather,
+	/// index i equals i + 1 count
+	/// </summary>
+	[Tooltip("random weigth for resource count to cost for resolving weather, index i equals i + 1 count")]
+	public List<int> CostCountWeigthList;
+}
