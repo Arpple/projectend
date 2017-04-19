c4a38d6e3d876482dab348b0e9290965ced63520
Arpple <rbo_sqat@hotmail.com>
2017-04-19 21:25:25 +0700

    game end when mission complete

---
+++ Assets/Scripts/_Sources/Mission/_System/MainMissionCompleteGameEndSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entitas;
+
+public class MainMissionCompleteGameEndSystem : GameReactiveSystem
+{
+	private GameEventContext _eventContext;
+
+	public MainMissionCompleteGameEndSystem(Contexts contexts) : base(contexts)
+	{
+		_eventContext = contexts.gameEvent;
+	}
+
+	protected override Collector<GameEntity> GetTrigger(IContext<GameEntity> context)
+	{
+		return context.CreateCollector(GameMatcher.MainMissionCompleted);
+	}
+
+	protected override bool Filter(GameEntity entity)
+	{
+		return entity.isMainMissionCompleted;
+	}
+
+	protected override void Execute(List<GameEntity> entities)
+	{
+		var eventEntity = _eventContext.CreateEntity();
+		eventEntity.isEventEndGame = true;
+	}
+}
