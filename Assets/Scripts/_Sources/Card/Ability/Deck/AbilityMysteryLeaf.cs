1e6d20b3fefac69c022723dd3ef268cbc6083894
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2017-04-23 19:41:03 +0700

    add mystery left ability and sprite

---
+++ Assets/Scripts/_Sources/Card/Ability/Deck/AbilityMysteryLeaf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class AbilityMysteryLeaf : ActiveAbility<UnitEntity>
+{
+    
+    public override TileEntity[] GetTilesArea(UnitEntity caster)
+	{
+		return TileAreaSelector.GetAllInRange(caster.GetTileOfUnit(),caster.unitStatus.VisionRange);
+	}
+
+    public override UnitEntity GetTargetFromSelectedTile(UnitEntity caster, TileEntity tile) {
+        return GetEnemyUnitFromTile(caster, tile);
+    }
+
+    public override void OnTargetSelected(UnitEntity caster, UnitEntity target) {
+        if(target.isDead) {
+            target.isDead = false;
+            target.RecoverHitpoint(1);
+        } else {
+            target.RecoverHitpoint(3);
+        }
+    }
+
+}
