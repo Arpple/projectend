fef475290fd200760dc6219ead90a14f96ac82d2
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2017-04-23 19:39:33 +0700

    Add Render Sprite Dead/Resurrection System

---
+++ Assets/Scripts/_Sources/Unit/_System/RenderDeadSpriteSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+public class RenderDeadSpriteSystem: ReactiveSystem<UnitEntity> {
+
+    private Sprite _deadSprite;
+    public RenderDeadSpriteSystem(Contexts contexts , UnitSetting unitSetting) : base(contexts.unit) {
+        this._deadSprite = unitSetting.DeadSprite;
+    }
+    
+    protected override bool Filter(UnitEntity entity) {
+        return entity.isDead;
+    }
+
+    protected override Collector<UnitEntity> GetTrigger(IContext<UnitEntity> context) {
+        return context.CreateCollector<UnitEntity>(UnitMatcher.Dead, GroupEvent.Added);
+    }
+
+    protected override void Execute(List<UnitEntity> entities) {
+        foreach(var unit in entities) {
+            Debug.Log(unit.view.GameObject.name + " is dead ");
+            unit.view.GameObject.GetComponent<SpriteRenderer>().sprite = this._deadSprite;
+        }
+    }
+}
---
+++ Assets/Scripts/_Sources/Unit/_System/RenderResurrectionSpriteSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+public class RenderResurrectionSpriteSystem: ReactiveSystem<UnitEntity> {
+    
+    public RenderResurrectionSpriteSystem(Contexts contexts) : base(contexts.unit) {
+
+    }
+    
+    protected override bool Filter(UnitEntity entity) {
+        return !entity.isDead;
+    }
+
+    protected override Collector<UnitEntity> GetTrigger(IContext<UnitEntity> context) {
+        return context.CreateCollector<UnitEntity>(UnitMatcher.Dead, GroupEvent.Removed);
+    }
+
+    protected override void Execute(List<UnitEntity> entities) {
+        foreach(var unit in entities) {
+            Debug.Log(unit.view.GameObject.name + " is Resurrection ");
+            unit.view.GameObject.GetComponent<SpriteRenderer>().sprite = unit.sprite.Sprite;
+        }
+    }
+}
+
