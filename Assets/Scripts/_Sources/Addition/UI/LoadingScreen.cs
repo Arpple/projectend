d8c34a60151a7ec130c715bc5ad1c7b8c4978d4b
VAYASAN\Vayasan <vayafamiglia@gmail.com>
2017-04-24 14:34:46 +0700

    Loading Screen

---
+++ Assets/Scripts/_Sources/Addition/UI/LoadingScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour{
+    public Text LoadingText;
+    private int count;
+    private float startTime = 0;
+    void Update() {
+        if(Time.time > startTime+1f) {
+            startTime = Time.time;
+            this.LoadingText.text = "Loading" + (count == 0 ? "" : (count == 1 ? " ." : (count == 2 ? " .." : " ...")));
+            count = (count + 1) % 4;
+        }
+    }
+}
