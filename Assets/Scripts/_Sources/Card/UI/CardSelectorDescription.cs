c573ca6754b4721896245736ed6161ca4b240b01
Arpple <rbo_sqat@hotmail.com>
2017-04-25 15:02:07 +0700

    add description when discard

---
+++ Assets/Scripts/_Sources/Card/UI/CardSelectorDescription.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardSelectorDescription : MonoBehaviour
+{
+	public Text DescriptionText;
+
+	private string _mainDescription;
+	private int _selectedCard;
+	private int _targetCount;
+
+	public void ShowDescription(string description, int targetCount)
+	{
+		_mainDescription = description;
+		_selectedCard = 0;
+		_targetCount = targetCount;
+		gameObject.SetActive(true);
+		UpdateDescription();
+	}
+
+	public void Hide()
+	{
+		DescriptionText.text = "";
+		gameObject.SetActive(false);
+	}
+
+	public void AddSelectedCard()
+	{
+		_selectedCard += 1;
+		UpdateDescription();
+	}
+
+	public void RemoveSelectedCard()
+	{
+		_selectedCard -= 1;
+		UpdateDescription();
+	}
+
+	private void UpdateDescription()
+	{
+		DescriptionText.text = string.Format("{0}{1}{2}",
+			_mainDescription,
+			Environment.NewLine,
+			string.Format("[{0}/{1}]", _selectedCard, _targetCount)
+		);
+	}
+
+
+}
